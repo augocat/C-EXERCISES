7a1849014be5459b2effbad221eeb7da67281278
narb <narborough.grahame@icloud.com>
2024-08-23 16:29:22 +1000

    setup of directories and files of 7B_STRINGS

---
+++ 7B_STRINGS/3 Character Access and Modification/exercise3.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  char p[] = "procezzor";
+  // Checkpoint 1 code goes here
+  // Checkpoint 2 code goes here
+  printf("%s", p);
+}
---
+++ 7B_STRINGS/5 Concatenating Strings/exercise5.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    
+  char s1[] = "London";
+  char s2[] = " Bridge";
+
+  char n[] = "New";
+  char y[] = " York";
+  char c[] = " City";
+
+  // Code for checkpoint 1 goes here
+  
+  // Code for checkpoint 2 goes here
+
+}
