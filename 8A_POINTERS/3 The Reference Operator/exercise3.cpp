f52bbf3e28648aab2167bd054a345724d9f05136
narb <narborough.grahame@icloud.com>
2024-08-25 09:03:17 +1000

    setup of directories and files of 8A_POINTERS

---
+++ 8A_POINTERS/3 The Reference Operator/exercise3.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+  
+  double g = 9.81;
+  double pi = 3.14;
+  
+  // Checkpoint 1 code goes here.
+
+  // Checkpoint 2 code goes here.
+
+  // Checkpoint 3 code goes here.
+  
+}
---
+++ 8A_POINTERS/5 Pointer Arithmetic/exercise5.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+  double* ptr1;
+
+  // Code for Checkpoint 1 goes here:
+
+  // Code for Checkpoint 2 goes here:  
+}
