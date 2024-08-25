4f672c5282e19a5b4366a25d290234087a09e65b
narb <narborough.grahame@icloud.com>
2024-08-25 16:40:47 +1000

    setup of directory and file of STRING_COPIER

---
+++ 8A_POINTERS/STRING_COPIER/_test.py
@@ -0,0 +1,17 @@
+//STRING_COPIER
+load_file_in_context('script.py')
+try:
+  PotatoSalad
+except NameError:
+  fail_tests("Is there a class called `PotatoSalad` defined?")
+try:
+  SpecialPotatoSalad
+except NameError:
+  fail_tests("Did you define a class called `SpecialPotatoSalad`?")
+
+try:
+  if not issubclass(SpecialPotatoSalad, PotatoSalad):
+    fail_tests("Does `SpecialPotatoSalad` subclass `PotatoSalad`?")
+except TypeError:
+  fail_tests("Are `PotatoSalad` and `SpecialPotatoSalad` both classes?")
+pass_tests()
---
+++ 8A_POINTERS/STRING_COPIER/stringCopy.cpp
@@ -0,0 +1,12 @@
+//STRING_COPIER
+#include<stdio.h>
+#include<string.h>
+ 
+void copy(char* dst, char* src){
+  // Code for copying a string goes here
+}
+ 
+int main(){
+  char srcString[] = "We promptly judged antique ivory buckles for the next prize!";
+ 
+}
