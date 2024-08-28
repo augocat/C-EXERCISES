85471dc75792c805f3a28d7919dd555adeb2f1c3
narb <narborough.grahame@icloud.com>
2024-08-29 08:36:26 +1000

    setup of directories and files of 10_STRUCTURES

---
+++ 10_STRUCTURES/1 Introduction/script.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  char firstName[] = "Ada";
+  char lastName[] = "Lovelace";
+  int age = 28;
+  char profession[] = "Mathematician";
+
+  printf("My name is %s %s, I am %d years old and I am a %s.", firstName, lastName, age, profession);
+}
